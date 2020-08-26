91b72ebe23ac773ea2651d116c8161986e635311
Vlad Bond <foolowin@gmail.com>
2020-08-26 13:33:21 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-$(document).ready(function ()(
-    $('.header__burger').click(function (_event)(
-        $('.header__burger,.header_menu').toggleClass('active');
-    ))
-))
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
